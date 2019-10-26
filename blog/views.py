80e3ee9af413c618c05d19fbba6a5298dbb86d64
bratyn20 <bratyn20@gmail.com>
2019-10-26 21:38:29 +0700

    new

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from .models import Post
+
 def post_list(request):
-    names = ['Oleg', 'Masha', 'Olja']
-    return render(request, 'blog/index.html', context={'names': names})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
