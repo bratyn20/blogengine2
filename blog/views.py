c57e85490cb2f0510bba6c7fc1547bd087bb0a5b
bratyn20 <bratyn20@gmail.com>
2019-10-27 14:31:35 +0700

    add Tags

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-from .models import Post
+from .models import Post, Tag
 
 def post_list(request):
     posts = Post.objects.all()
@@ -11,3 +11,11 @@ def post_list(request):
 def post_detail(request, slug):
     post = Post.objects.get(slug__iexact=slug)
     return render(request, 'blog/post_detail.html', context={'post': post})
+
+def tags_list(request):
+    tags= Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(request, 'blog/tag_detail.html', context={'tag': tag})
